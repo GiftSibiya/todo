2205b17d709a2e1e281063ab5e3b4784e51ec28e
GiftSibiya <sibiya.bobo@gmail.com>
2023-12-30 19:15:45 +0200

    frontend: fetch using axios

--- tasker/src/App.js
+++ tasker/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/");
+        setTasks(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task List</h1>
+
+      {tasks.map((task, index) => (
+        <>
+          <div>
+            <li key={index}>{task.title}</li>
+            <p key={index}>{task.bodyText}</p>
+          </div>
+        </>
+      ))}
     </div>
   );
 }
